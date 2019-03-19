ec64757607cc4c87e8846cc0c1c9f463fbfa613c
Jason Springer <jsnsprngr@gmail.com>
2019-03-19 18:55:40 -0400

    Update title

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module JobBoard
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
-    config.application_name = 'JOB BOARD'
+    config.application_name = 'techJobs'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
